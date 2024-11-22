b2946d1cb0f5e492bda130edd1f80996390e76e6
Paulo Vitor dos Santos <paulo.santos@run2biz.com>
2024-11-22 12:35:48 -0300

    test: Marketplace API

---
+++ marketplace/fetch.js
@@ -0,0 +1,38 @@
+async function fetchService(endpoint, method = "GET", data = null) {
+    try {
+        const url = `${document.querySelector("#base-url").value}/${
+            document.querySelector("#api-version").value
+        }${endpoint}`;
+
+        const response = await fetch(url, {
+            method,
+            body: data ? JSON.stringify(data) : null,
+            headers: {
+                ["Content-Type"]: "application/json",
+                ["api-key"]: document.querySelector("#api-key").value
+            },
+            credentials: "include"
+        });
+
+        const result = {
+            status: response.status,
+            data: !response.headers.get("Content-Type")?.includes("json")
+                ? !response.headers.get("Content-Type")?.includes("text")
+                    ? null
+                    : await response.text()
+                : await response.json()
+        };
+
+        if (result.status >= 400) {
+            throw { response: result };
+        }
+
+        return result;
+    } catch (error) {
+        console.error(error);
+        return {
+            status: error.response?.status || 500,
+            data: error.response?.data || error.message || error
+        };
+    }
+}
